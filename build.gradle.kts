8b78022fe0e45f6fa3593e8b3e02b8cef1b6315c
Artem Kraus <pr0gger.dev@gmail.com>
2026-06-27 21:46:29 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,77 @@
+plugins {
+	kotlin("jvm") version "2.2.21"
+	kotlin("plugin.spring") version "2.2.21"
+	id("org.springframework.boot") version "4.0.1"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.liquibase.gradle") version "2.2.0"
+	kotlin("plugin.jpa") version "2.2.21"
+}
+
+group = "com.app"
+version = "0.0.1-SNAPSHOT"
+description = "spring-boot-kotlin-template"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-liquibase")
+	implementation("org.springframework.boot:spring-boot-starter-restclient")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("tools.jackson.module:jackson-module-kotlin")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+
+	runtimeOnly("org.postgresql:postgresql")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-data-rest-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-jdbc-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-liquibase-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-restclient-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testRuntimeOnly("com.h2database:h2")
+
+	implementation("org.liquibase:liquibase-core")
+	liquibaseRuntime("org.liquibase:liquibase-core:5.0")
+	liquibaseRuntime("org.liquibase:liquibase-groovy-dsl:2.1.1")
+	liquibaseRuntime("org.yaml:snakeyaml:2.0")
+	liquibaseRuntime("org.postgresql:postgresql")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+	}
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
+tasks {
+	withType<Test> {
+		useJUnitPlatform()
+	}
+
+	bootJar {
+		archiveFileName.set("${project.group}.${project.name}.jar")
+	}
+}
