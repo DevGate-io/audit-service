2beb66a2dda45a7cec1d152bab13883faa567658
pr0gger1 <pr0gger.dev@gmail.com>
2026-07-01 22:40:34 +0300

    fix: пометил интерфейс репозитория как бин spring ioc, сделал сортировку логов по дате создания по умолчанию

--- src/main/kotlin/com/devgate/audit/repositories/AuditLogRepository.kt
+++ src/main/kotlin/com/devgate/audit/repositories/AuditLogRepository.kt
@@ -2,5 +2,7 @@ package com.devgate.audit.repositories
 
 import com.devgate.audit.models.AuditLog
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface AuditLogRepository: JpaRepository<AuditLog, Long>
